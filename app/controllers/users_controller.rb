390fd27d62b2a3aee9eb935b6f387e4ce4cf5aaa
Ted Kang <tedtkang@gmail.com>
2015-02-06 13:15:23 -0800

    Added a few pages that need authentication and added smart rerouting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit]#, :update]
+  before_action :correct_user,   only: [:edit]#, :update]
+
+
   def show
     @user = User.find(params[:id])
   end
@@ -19,10 +23,31 @@ class UsersController < ApplicationController
     end    
   end
 
+  def edit
+    puts params
+    @user = User.find(params[:id])
+    puts @user.email
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:email, :password,
                                    :password_confirmation)
     end  
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end    
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
