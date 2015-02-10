d033d2dd05976567ebe659ce56ab75d90cfd83ef
Ted Kang <tedtkang@gmail.com>
2015-02-09 16:44:35 -0800

    Added account activation emails

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ source 'https://rubygems.org'
 gem 'rails', '4.2.0'
 # Bcrypt needed for has_secure_password
 gem 'bcrypt', '3.1.7'
+# For creating fake seed users
+gem 'faker', '1.4.2'
+
 # User sass with bootstrap for styling
 gem 'bootstrap-sass',       '3.2.0.0'
 # awesome print!
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(email: "tedtkang@gmail.com",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             activated: true,
+             activated_at: Time.zone.now)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "test-user-#{n+1}@gmail.com"
+  password = "password"
+  User.create!(email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
+end
